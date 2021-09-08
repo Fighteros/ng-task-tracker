31585a416c91627477cc974bccc035be7a01bf07
fighteros <fighteros121212@gmail.com>
2021-09-08 15:32:49 +0200

    added update task

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -11,13 +11,18 @@ export class TaskItemComponent implements OnInit {
   @Input() task!: Task;
   faTimes = faTimes;
   @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter()
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter()
 
   constructor() { }
 
   ngOnInit(): void {
   }
-  onDelete(task: any) {
+  onDelete(task: Task) {
     this.onDeleteTask.emit(task)
+  }
+
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task)
   }
 
 }
